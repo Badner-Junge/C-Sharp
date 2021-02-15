172cfd09578d44f73ac332d1df3b79eede92f126
Fabian <kraichtaler_junge@gmx.de>
2021-02-15 23:45:44 +0100

    Family Organizer hinzugef., Prog. Start. Video 6

--- ProgrammierenStarten/ProgrammierenStarten/Program.cs
+++ ProgrammierenStarten/ProgrammierenStarten/Program.cs
@@ -7,6 +7,7 @@ namespace ProgrammierenStarten
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("2te Zeile");
             Console.ReadKey();
         }
     }
