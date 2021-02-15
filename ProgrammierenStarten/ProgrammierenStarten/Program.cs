bb61dc099f12705d340d954abb64969cf77029e8
Badner-Junge <kraichtaler-junge@gmx.de>
2021-02-15 17:01:25 +0100

    Hallo Welt in Hello World geändert

--- ProgrammierenStarten/ProgrammierenStarten/Program.cs
+++ ProgrammierenStarten/ProgrammierenStarten/Program.cs
@@ -6,7 +6,7 @@ namespace ProgrammierenStarten
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hallo Welt!");
+            Console.WriteLine("Hello World!");
             Console.ReadKey();
         }
     }
