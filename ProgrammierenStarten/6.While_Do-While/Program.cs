fc0926d3cf1b51fffe17bc8ab16a1e619ba3111d
Fabian <kraichtaler_junge@gmx.de>
2021-02-17 23:15:01 +0100

    Programmieren Starten Crashkurs abgeschlossen

--- ProgrammierenStarten/6.While_Do-While/Program.cs
+++ ProgrammierenStarten/6.While_Do-While/Program.cs
@@ -6,7 +6,26 @@ namespace While_DoWhile
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //While-Schleifen
+            int zahl = 1;
+
+            while(zahl <= 5)
+            {
+                Console.WriteLine(zahl);
+                zahl += 1;
+            }
+
+            //Do-While-Schleife
+            int zahl2 = 7;
+
+            do
+            {
+                Console.WriteLine(zahl2);
+                zahl2 += 1;
+            }
+            while (zahl2 <= 5);
+
+            Console.ReadKey();
         }
     }
 }
